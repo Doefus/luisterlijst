01e2a3b97b192e03afc2db971060d78dbd96c042
Doefus <86236727+Doefus@users.noreply.github.com>
2023-07-14 16:24:45 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -3,7 +3,25 @@
 
 <div class="intro">
 <h3>Welkom bij de muziek luisterlijst!</h3><br>
-Gebruik dit project om te oefenen en te leren werken met SQL en PHP/HTML. Lees de readme.md file om te installeren.
+<p>
+
+
+</p>Gebruik dit project om te oefenen en te leren werken met SQL en PHP/HTML. Lees de readme.md file om te installeren.
+
+<h4>Onderzoeksvragen</h4>
+- Wat doet install.php?<br>
+- Wat genereert header.php precies?<br>
+- Welk bestand zorgt voor de styling?<br>
+- Zoek naar de query’s op de Luisterlijst pagina:<br>
+&nbsp;&nbsp;&nbsp;> Download de lijst <br>
+&nbsp;&nbsp;&nbsp;> Verwijder een entry <br>
+- Zoek de code op de Voeg nummer toe pagina: <br>
+&nbsp;&nbsp;&nbsp;> Die er voor zorgt dat een jaar altijd een integer is <br>
+&nbsp;&nbsp;&nbsp;> Dat een SQL verbinding verbroken wordt <br>
+&nbsp;&nbsp;&nbsp;> Die er voor zorgt dat een create-query alleen uitgevoerd wordt als de titel gevuld is <br>
+- Maak een lijst wat er allemaal voor nodig zou zijn wanneer je ook de Rating (schaal 1 tot 5) bij zou willen houden van een nummer <br>
+
+
 </div>
 
 
